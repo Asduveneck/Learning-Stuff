c672407c70d1d0f29fc51d6cb2e5eb7eef51fc6c
Asduveneck <asduveneck@ucdavis.edu>
2020-03-05 19:33:36 -0800

    Attempt text label for fun and by quick google search

--- D3/Mastering_data_viz_course/Provided/02/2.08.1/js/main.js
+++ D3/Mastering_data_viz_course/Provided/02/2.08.1/js/main.js
@@ -6,8 +6,8 @@
 
 var svg = d3.select("#chart-area")
     .append("svg")
-    .attr("width", "500")
-    .attr("height", "500");
+    .attr("width", "550")
+    .attr("height", "550");
 
 d3.json("data/buildings.json").then(function(data){
     data.forEach( d => {d.height = +d.height});
@@ -18,7 +18,19 @@ d3.json("data/buildings.json").then(function(data){
     rects.enter().append("rect")
       .attr("fill", "#672035")
       .attr("width", 40)
-      .attr("height", (d) => d.height)
-      .attr("x", (d, i) => 60*i)
+      .attr("height", (d) => d.height) // set height to value in data
+      .attr("x", (d, i) => 60*i) // Offset each one by 60
+      .attr("y", (d) => 500 - d.height ) // Offset by height of SVG to make bar graph align at bottom
+
+    let texts = svg.selectAll("text")
+      .data(data);
+
+    texts.enter().append("text")
+      .text((d) => d.name)
+      .attr("x", (d,i) => 80*i)
+      .attr("y",  (d, i) => 500 - 10*i) 
+      .attr('text-anchor', 'start')
+      .attr("transform", "rotate(10)")
+
     
 }).catch(error => console.log("Bug in promise at line 12.\nError:", error))
