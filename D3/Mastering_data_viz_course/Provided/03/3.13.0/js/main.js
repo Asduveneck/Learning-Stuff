691b0ae47662e3d89cbe0cd7521bcf6335028ece
Asduveneck <asduveneck@ucdavis.edu>
2020-03-10 23:21:29 -0700

    Define axis scales

--- D3/Mastering_data_viz_course/Provided/03/3.13.0/js/main.js
+++ D3/Mastering_data_viz_course/Provided/03/3.13.0/js/main.js
@@ -3,22 +3,38 @@
 *    Mastering Data Visualization with D3.js
 *    Project 1 - Star Break Coffee
 */
+
+// Setting Margins
 let margin = { lft: 40, rht: 20, top: 10, bot: 30 };
 margin.hz = margin.lft + margin.rht;
 margin.vt = margin.top + margin.bot;
-console.log(margin);
+
+// setting graph dimensions
 let width = 600 - margin.hz;
 let height = 400 - margin.vt;
 
+// setting chart dimensions
 let g = d3.select("#chart-area")
   .append("svg")
     .attr("width", `${width + margin.hz}`)
     .attr("height", `${height + margin.vt}`)
-  .append("g")
+  .append("g") // shift by margin for scale and labels
     .attr("transform", `translate(${margin.lft}, ${margin.top})`);
 
 d3.json("data/revenues.json").then( (data) => {
   // console.log(data) // month , revenue, profit
+  data.forEach( (d) => {
+    d.revenue = +d.revenue; // convert to string
+    d.profit = +d.profit; // convert to string
+  })
 
+  let x = d3.scaleBand()
+    .domain(data.map( (d) => d.month))
+    .range([0, width])
+    .paddingInner(0.3);
+
+  let y = d3.scaleLinear()
+    .domain([0, d3.max(data, (d) => d.revenue)])
+    .range([height, 0]);
 
 });
